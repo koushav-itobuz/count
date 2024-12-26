4ba7e619082cca85a0d4626372371487d8611a70
Koushav Sarkar <koushav@itobuz.com>
2024-12-26 18:34:35 +0530

    task completed

---
+++ main.js
@@ -0,0 +1,30 @@
+const num1 = document.getElementById("num1");
+let val = Number (num1.innerText);
+console.log(typeof(val));
+
+
+function reset () {
+  val = 0;
+  num1.innerHTML = val;
+  update();
+}
+
+function increase () {
+  val += 1;
+  num1.innerHTML= val;
+  update();
+ 
+}
+
+function decrease () {
+  val -= 1;
+  num1.innerHTML = val;
+  update();
+  
+}
+
+function update () {
+  if (val > 0) num1.style.color="green";
+  else if (val < 0) num1.style.color="red";
+  else if (val === 0) num1.style.color="black";
+}
